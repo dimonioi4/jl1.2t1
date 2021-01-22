c790c4867f6a8ced754a2b985c8effdc2e21481c
dimonioi4 <dimonioi4@yandex.ru>
2021-01-22 12:03:06 +0300

    added money transfer

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int amount = 500_000_000;
+        int total = amount + balance;
+        System.out.println(total);
+    }
+}
